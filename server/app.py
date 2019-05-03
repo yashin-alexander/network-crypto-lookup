2b645516c33b46e20be72d618bcf2f60d58709ae
Alexander Yashin <alexandr.yashin@emlid.com>
2019-05-03 12:52:40 +0300

    server: app: delegate all lookup logic to lookup_manager

--- server/app.py
+++ server/app.py
@@ -1,14 +1,9 @@
-import requests
 import json
 
 from flask import Flask, render_template
 from ecies.utils import generate_key
-from ecies import decrypt
 
 from lookup_manager import lookup
-
-
-CLIENT_APP_PORT = 5000
 
 
 def get_keys():
@@ -20,7 +15,6 @@ def get_keys():
 
 app = Flask(__name__)
 prvkey, pubkey = get_keys()
-network_ips = []
 machine_identifers = {}
 
 
@@ -29,40 +23,8 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/lookup_ips')
-def lookup_ips():
-    network_ips = lookup()
-    return "Some ip's detected: {}".format(network_ips)
-
-
-def get_machine_identifer(ip):
-    global prvkey, pubkey
-    url = 'http://{}:{}/encrypted_identifer'.format(ip, CLIENT_APP_PORT)
-    try:
-        response = requests.post(url,
-                                 data=pubkey.encode(),
-                                 headers={'Content-Type': 'application/octet-stream'},
-                                 timeout=5)
-    except requests.exceptions.ConnectionError:
-        return None
-    if not response.ok:
-        return None
-    encrypted_machine_identifer = response.content
-    machine_identifer = decrypt(prvkey, encrypted_machine_identifer)
-    return machine_identifer
-
-
 @app.route('/lookup_machines', methods=['GET'])
-def lookup_machines():
-    network_ips = lookup()
-    machine_identifers = {}
-    print("Networks detected: {}".format(network_ips))
-    for ip in network_ips:
-        machine_identifer = get_machine_identifer(ip)
-        if not machine_identifer:
-            print('Failed to connect to {}'.format(ip))
-            continue
-        print('{} connection successfull'.format(ip))
-        decoded_identifer = machine_identifer.decode()
-        machine_identifers.update({ip: decoded_identifer})
-    return json.dumps(machine_identifers)
+def get_machine_identifer():
+    global pubkey, prvkey
+    lookup_results = lookup(pubkey, prvkey)
+    return json.dumps(lookup_results)
