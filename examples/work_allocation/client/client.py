0e1cfa185d1de851916b39c3a7124a6f2293bcab
Alexander Yashin <alexandr.yashin@emlid.com>
2019-05-05 14:45:21 +0300

    work_allocation: client: add device_state\process_work methods

--- examples/work_allocation/client/client.py
+++ examples/work_allocation/client/client.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, abort, jsonify
-from ecdh import DiffieHellman, load_pem_pubkey
+import json
+import time
+import threading
+from ecdh import DiffieHellman, load_pem_pubkey, from_base64_dict, to_base64_dict
 
 
 app = Flask(__name__)
@@ -19,9 +22,37 @@ def assign_server_pubkey():
     return jsonify(payload), 200
 
 
-@app.route('/assign_id', methods=['POST'])
-def assign_id():
-    server_pubkey = request.data.decode()
+@app.route('/device_state', methods=['GET'])
+def device_state():
+    global ecdh, server_pubkey, work_processing
     if not server_pubkey:
         abort(400)
-    return ''
+    encrypted_message = ecdh.encrypt(server_pubkey, str(work_processing))
+    raw_dict = {'iv': ecdh.IV, 'payload': encrypted_message}
+    payload = to_base64_dict(raw_dict)
+    return jsonify(payload), 200
+
+
+@app.route('/process_work', methods=['POST'])
+def process_work():
+    global ecdh, server_pubkey, work_processing
+    encoded_dict = json.loads(request.data)
+    decoded_dict = from_base64_dict(encoded_dict)
+    try:
+        work_delay = ecdh.decrypt(server_pubkey,
+                                  decoded_dict.get('payload'),
+                                  decoded_dict.get('iv'))
+    except TypeError:
+        abort(400)
+    if work_processing:
+        abort(400)
+    worker = threading.Thread(target=_work, args=(int(work_delay), ))
+    worker.start()
+    return '', 200
+
+
+def _work(delay):
+    global work_processing
+    work_processing = True
+    time.sleep(delay)
+    work_processing = False
